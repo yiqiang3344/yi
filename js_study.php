7b4f516910b0f8307f810fa3c577a2e1becbe7d3
sidney <yiqiang3344@gmail.com>
2013-07-05 09:12:26 +0800

    object js

--- js_study.php
+++ js_study.php
@@ -4,6 +4,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>test3.html</title>
         <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/my.js"></script>
     </head>
     <body>
         <!-- <div><input type="text" id="content" placeholder="输入要检测的文字"/></div>
@@ -15,17 +16,83 @@
             <option value="2">2</option>
             <option value="3">3</option>
         </select> -->
-        <div id="info" class="cl1 cl2">test
+        <!-- <div id="info" class="cl1 cl2">test
         </div>
-        <div><button id="check">检测</button></div>
+        <div><button id="check">检测</button></div> -->
+
+        <!-- <<div id="info"></div> -->
 
         <script type="text/javascript">
+            // $("#info").html("<script>alert(1);<\/script>");
+            // var Name = Class.extend({
+            //     init: function(name) {
+            //         this.name = 'Yi' + name;
+            //     }
+            // });
+            // var NamePrinter = Name.extend({
+            //     showName: function() {
+            //         alert(this.name);
+            //     },
+            //     init:function(name){
+            //         this._super(name);
+            //         this.name += name;
+            //     }
+            // });
+
+            // var yjq = new NamePrinter('qiang');
+            // yjq.showName();
+
+
+            function Name(firstName){
+                this.init = function( name){
+                    this.name = name;
+                }
+                // this.init(firstName);
+            }
+
+            function NamePrinter(name){
+                this.showName = function(){
+                    alert(this.name);
+                }
+                var super_init = this.init;
+                this.init = function(name){
+                    super_init.call(this,name);
+                    this.name += name;
+                }
+                this.init(name);
+            }
+            NamePrinter.prototype = new Name('Yi');
+
+            // NamePrinter.init = function(name){
+            //     this.name = name;
+            // }
+            var yjq = new NamePrinter('qiang');
+
+            yjq.showName();
+
+
+
+
+            // var animals = [
+            //   {species: 'Lion', name: 'King'},
+            //   {species: 'Whale', name: 'Fail'}
+            // ];
+             
+            // for (var i = 0; i < animals.length; i++) {
+            //   (function (i) {
+            //     this.print = function () {
+            //       console.log('#' + i  + ' ' + this.species + ': ' + this.name);
+            //     }
+            //     this.print();
+            //   }).call(animals[i], i);
+            // }
+
             // $('#info').delegate('p','click',{t:'test'},function(e){
             //     console.log(e.data);
             //     $(this).text('test');
             // });
-            $('#check').click(function(){
-                $(this).unwrap();
+            // $('#check').click(function(){
+            //     $(this).unwrap();
                 // $('#info').append('<p style="background-color:yellow;height:50px;width:100px;"></p>');
                 // $.ajax({
                 //     xhr:function(){
@@ -71,7 +138,7 @@
                 //         return 'cl1';
                 //     // }
                 // });
-            });
+            // });
             // var list = [];
             // list.test = 1;
             // console.log(list.test);
